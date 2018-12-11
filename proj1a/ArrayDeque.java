b30729553a1fc9f949f0cbe410b3e6ea44db7c83
zhidong li <2859208791@qq.com>
2018-12-11 23:12:54 +0800

    modify

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -4,97 +4,104 @@ public class ArrayDeque<T> {
     private int nextFirst;
     private int nextLast;
 
-    public ArrayDeque(){
+    public ArrayDeque() {
         array = (T[]) new Object[8];
         nextFirst = 0;
         nextLast = 1;
         size = 0;
     }
-    private int minusOne(int index){
+    private int minusOne(int index) {
         int indexOfMinusOne;
-        if(index==0)    indexOfMinusOne=array.length-1;
-        else indexOfMinusOne=index-1;
+        if (index == 0) {
+            indexOfMinusOne = array.length - 1;
+        } else {
+            indexOfMinusOne = index - 1;
+        }
         return indexOfMinusOne;
     }
-    private int plusOne(int index){
+    private int plusOne(int index) {
         int indexOfPlusOne;
-        if(index==(array.length-1)) {
+        if (index == (array.length - 1)) {
             indexOfPlusOne = 0;
+        } else {
+            indexOfPlusOne = index + 1;
         }
-        else indexOfPlusOne=index+1;
         return indexOfPlusOne;
     }
-    public void addFirst(T item){
-        if(isFull()==true){
-            T[] ResizeArray=(T[]) new Object[array.length*2];
-            int copyIndex=1;
-            for(int i=nextFirst+1;i!=nextLast;i=(i+1)%array.length){
-                ResizeArray[copyIndex++]=array[i];
+    public void addFirst(T item) {
+        if (isFull()) {
+            T[] resizeArray = (T[]) new Object[array.length * 2];
+            int copyIndex = 1;
+            int i = nextFirst + 1;
+            for (; i != nextLast; i = (i + 1) % array.length) {
+                resizeArray[copyIndex++] = array[i];
             }
-            nextFirst=0;
-            nextLast=array.length;
-            array=ResizeArray;
+            nextFirst = 0;
+            nextLast = array.length - 1;
+            array = resizeArray;
         }
-        array[nextFirst]=item;
-        nextFirst=minusOne(nextFirst);
-        size=size+1;
+        array[nextFirst] = item;
+        nextFirst = minusOne(nextFirst);
+        size = size + 1;
     }
 
-    public void addLast(T item){
-        if(isFull()==true){
-            T[] ResizeArray=(T[]) new Object[array.length*2];
-            int copyIndex=1;
-            for(int i=nextFirst+1;i!=nextLast;i=(i+1)%array.length){
-                ResizeArray[copyIndex++]=array[i];
+    public void addLast(T item) {
+        if (isFull()) {
+            T[] resizeArray = (T[]) new Object[array.length * 2];
+            int copyIndex = 1;
+            int i = nextFirst + 1;
+            for (; i != nextLast; i = (i + 1) % array.length) {
+                resizeArray[copyIndex++] = array[i];
             }
-            nextFirst=0;
-            nextLast=array.length-1;
-            array=ResizeArray;
+            nextFirst = 0;
+            nextLast = array.length - 1;
+            array = resizeArray;
         }
-        array[nextLast]=item;
-        nextLast=plusOne(nextLast);
-        size=size+1;
+        array[nextLast] = item;
+        nextLast = plusOne(nextLast);
+        size = size + 1;
     }
 
-    public boolean isEmpty(){
-        return (nextFirst+1)%array.length==nextLast;
+    public boolean isEmpty() {
+        return (nextFirst + 1) % array.length == nextLast;
     }
-    public boolean isFull(){
-        return (nextLast+1)%array.length==nextFirst;
+    private boolean isFull() {
+        return (nextLast + 1) % array.length == nextFirst;
     }
-    public int size(){
+    public int size() {
         return size;
     }
-    public void printDeque(){
-        if(isEmpty()==false) {
-            for (int i = nextFirst + 1; i != nextLast; i = plusOne(i))
+    public void printDeque() {
+        if (!isEmpty()) {
+            for (int i = nextFirst + 1; i != nextLast; i = plusOne(i)) {
                 System.out.println(array[i]);
+            }
         }
     }
-    public T removeFirst(){
-        T removeItem=null;
-        if(isEmpty()==false){
-            removeItem=array[plusOne(nextFirst)];
-            array[plusOne(nextFirst)]=null;
-            nextFirst=plusOne(nextFirst);
-            size=size-1;
-        }
-        return removeItem;
-    }
-    public T removeLast(){
-        T removeItem=null;
-        if(isEmpty()==false){
-            removeItem=array[minusOne(nextLast)];
-            array[minusOne(nextLast)]=null;
-            nextLast=minusOne(nextLast);
-            size=size-1;
+    public T removeFirst() {
+        T removeItem = null;
+        if (!isEmpty()) {
+            removeItem = array[plusOne(nextFirst)];
+            array[plusOne(nextFirst)] = null;
+            nextFirst = plusOne(nextFirst);
+            size = size - 1;
         }
         return removeItem;
     }
-    public T get(int index){
-        T getItem=null;
-        if(isEmpty()==false){
-            getItem=array[(nextFirst+index)%array.length];
+    public T removeLast() {
+        T removeItem = null;
+        if (!isEmpty()) {
+            removeItem = array[minusOne(nextLast)];
+            array[minusOne(nextLast)] = null;
+            nextLast = minusOne(nextLast);
+            size = size - 1;
+        }
+        return removeItem;
+    }
+    public T get(int index) {
+        T getItem = null;
+        if (!isEmpty()) {
+            getItem = array[(nextFirst + index + 1) % array.length];
         }
         return getItem;
     }
