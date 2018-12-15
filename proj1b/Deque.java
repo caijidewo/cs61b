61c2d30d47c205d5ec9845a538b03a1dbb7ce30f
zhidong li <2859208791@qq.com>
2018-12-16 00:00:35 +0800

    modify

--- proj1b/Deque.java
+++ proj1b/Deque.java
@@ -1,10 +1,10 @@
 public interface Deque<Item> {
-    public void addFirst(Item item);
-    public void addLast(Item item);
-    public boolean isEmpty();
-    public int size();
-    public void printDeque();
-    public Item removeFirst();
-    public Item removeLast();
-    public Item get(int index);
+    void addFirst(Item item);
+    void addLast(Item item);
+    boolean isEmpty();
+    int size();
+    void printDeque();
+    Item removeFirst();
+    Item removeLast();
+    Item get(int index);
 }
