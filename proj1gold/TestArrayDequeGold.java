43a09a4b81522c3f958ba9dfef6a336e8fc73431
zhidong li <2859208791@qq.com>
2018-12-19 12:39:59 +0800

    add TestArrayDequeGold.java

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -32,4 +32,7 @@ public class TestArrayDequeGold {
             }
         }
     }
+    public static void main(String[] args) {
+        jh61b.junit.TestRunner.runTests(TestArrayDequeGold.class);
+    }
 }
