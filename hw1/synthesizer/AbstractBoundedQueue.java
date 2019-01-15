d641df0048ce8692488214d9140003da18aae03c
zhidong li <2859208791@qq.com>
2019-01-15 14:08:27 +0800

    modify

---
+++ hw1/synthesizer/AbstractBoundedQueue.java
@@ -0,0 +1,28 @@
+package synthesizer;
+
+public abstract class AbstractBoundedQueue<T> implements BoundedQueue<T> {
+    protected int fillCount;
+    protected int capacity;
+    @Override
+    public int capacity() {
+        return capacity;
+    }
+    @Override
+    public int fillCount() {
+        return fillCount;
+    }
+    @Override
+    public boolean isEmpty() {
+        return fillCount == 0;
+    }
+    @Override
+    public boolean isFull() {
+        return fillCount == capacity;
+    }
+    @Override
+    public abstract T peek();
+    @Override
+    public abstract T dequeue();
+    @Override
+    public abstract void enqueue(T x);
+}
---
+++ hw1/synthesizer/BoundedQueue.java
@@ -0,0 +1,20 @@
+package synthesizer;
+import java.util.Iterator;
+public interface BoundedQueue<T> extends Iterable<T> {
+    /** return size of the buffer. */
+    int capacity();
+    /** return number of items currently in the buffer. */
+    int fillCount();
+    /** add item x to the end. */
+    void enqueue(T x);
+    /** delete and return item from the front. */
+    T dequeue();
+    /** return (but do not delete) item from the front. */
+    T peek();
+    default boolean isEmpty() {
+        return fillCount() == 0;
+    }
+    default boolean isFull() {
+        return capacity() == fillCount();
+    }
+}
