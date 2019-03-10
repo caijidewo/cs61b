5e31387d644e1e9bf30a651967a7facef6830dfb
zhidong li <2859208791@qq.com>
2019-03-10 23:17:36 +0800

    modify

--- hw3/hw3/hash/TestSimpleOomage.java
+++ hw3/hw3/hash/TestSimpleOomage.java
@@ -78,7 +78,7 @@ public class TestSimpleOomage {
             oomages.add(SimpleOomage.randomSimpleOomage());
         }
 
-        assertTrue(OomageTestUtility.haveNiceHashCodeSpread(oomages, 10));
+        assertTrue(OomageTestUtility.haveNiceHashCodeSpread(oomages, 9));
     }
 
     /** Calls tests for SimpleOomage. */
