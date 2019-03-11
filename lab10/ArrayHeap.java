9c2946a8e90dac3b8b61928f7f751710c723e163
zhidong li <2859208791@qq.com>
2019-03-11 18:44:24 +0800

    add ArrayHeap.java

--- lab10/ArrayHeap.java
+++ lab10/ArrayHeap.java
@@ -259,8 +259,7 @@ public class ArrayHeap<T> implements ExtrinsicPQ<T> {
             throw new IllegalArgumentException("Cannot sink or swim nodes with index 0 or less");
         }
         if (index > size) {
-            throw new IllegalArgumentException("Cannot sink or swim nodes "
-                    +  "with index greater than current size.");
+            throw new IllegalArgumentException("Cannot sink or swim nodes with index greater than current size.");
         }
         if (contents[index] == null) {
             throw new IllegalArgumentException("Cannot sink or swim a null node.");
